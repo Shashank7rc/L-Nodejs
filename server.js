9cd3c8bd68410cc56c04f1481d02bf6ff3f0940e
Shashank Singh <shashank.rajput2k17@gmail.com>
2026-06-09 19:34:56 +0530

    Authentication using passport.js version 1.0

--- server.js
+++ server.js
@@ -3,12 +3,40 @@ const app = express();
 const port = 3000;
 const db =require('./db');
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const Person=require('./models/person');
 
-app.get('/', (req, res) => {
+//authentication logic
+passport.use(new LocalStrategy(async (username,password,done) => {
+  try{
+      console.log("receive credentials",USERNAME,PASSWORD);
+      const user = await Person.findOne({username:USERNAME});
+      if(!user)
+          return done(null,false,{message : 'Innocrect username'});
+      const isPasswordmatch = user.PASSWORD == password ?true:false;
+      if(!isPasswordmatch){
+          return done(null,user);
+      }
+      else{
+         return done(null,false,{message:'Incoorect password'})
+      }
+  }catch(err){
+      return done(err);
+  }
+}))
+
+app.use(passport.initialize());
+
+
+const localAuthMiddleware=passport.authenticate('local',{session: false})
+app.get('/',localAuthMiddleware, (req, res) => {
   res.send('Hello World, Welcome to hotel!');
 });
 
